22453a2983b60baf22d2a630baf02f55a58ade41
Ugur Can Avcu <59328866+ugurcanavcu@users.noreply.github.com>
2021-11-09 23:26:52 -0500

    Update SoccerBall.java
    adding comments for javadoc

--- src/model/SoccerBall.java
+++ src/model/SoccerBall.java
@@ -13,16 +13,22 @@ public class SoccerBall {
 	private double velocity;
 
 	private final Color color;
-
+	/**
+	 * the constructor to initialize the soccerball
+	 */
 	private SoccerBall() {
 		color = Color.white;
 		resetSoccerBall();
 	}
-
+	
+	/**
+	* returning static the soccerBall object
+	* @return soccerBall
+	*/
 	public static SoccerBall getSoccerBall() {
 		return soccerBall;
 	}
-
+	
 	public void moveBall(int initialDistance, double initialVelocity, double acceleration) {
 		moveBallY(initialDistance);
 		setVelocity(initialVelocity);
@@ -49,32 +55,58 @@ public class SoccerBall {
 			setVelocity(0.0);
 		}
 	}
-
+	
+	/**
+	* resetting velocity and position of the ball 
+	*/
 	public void resetSoccerBall() {
 		setVelocity(0.0);
 		setPosition(new Point(480, 500));
 	}
-
+	/**
+	* checks whether ball is on goalkeeper side
+	* @return true, if the ball is on goalkeeper side
+	*	  false, if the ball is not on goalkeeper side
+	*/
 	public boolean onGoalkeeperSide() {
 		return getPosition().y < 200;
 	}
-
+	/**
+	* checks whether ball is in Gate
+	* @return true, if the ball is in Gate
+	*	  false, if the ball is not in Gate
+	*/
 	public boolean inGate() {
 		return getPosition().x > 180 && getPosition().x < 400 && getPosition().y > 10 && getPosition().y < 60;
 	}
-
+	/**
+	* sets the velocity of ball object 
+	* @param double velocity
+	*/
 	public void setVelocity(double velocity) {
 		this.velocity = velocity;
 	}
-
+	
+	/**
+	* getting the position of the ball, returning the position of the ball
+	* @return position
+	*/
 	public Point getPosition() {
 		return position;
 	}
-
+	
+	/**
+	* sets the position of ball object 
+	* @param Point position
+	*/
 	public void setPosition(Point position) {
 		this.position = position;
 	}
-
+	
+	/**
+	* getting the color of the ball, returning the color of the ball
+	* @return color
+	*/
 	public Color getColor() {
 		return color;
 	}
