c66531561766105b555faef0a4341f89be439bc5
Ugur Can Avcu <59328866+ugurcanavcu@users.noreply.github.com>
2021-11-09 23:07:57 -0500

    Update Goalkeeper.java
    Adding some comment to classes for javadoc

--- src/model/players/Goalkeeper.java
+++ src/model/players/Goalkeeper.java
@@ -8,45 +8,65 @@ import java.util.Random;
 public class Goalkeeper extends GamePlayer {
 
 	private int movementStep;
-
+	
+	/**
+	 * the constructor to initialize the attributes of the goalkeeper
+	 * @param String name, Color color
+	 */
 	public Goalkeeper(String name, Color color) {
 		super(name, color);
 		movementStep = 10;
 	}
-
+	
+	/**
+	 * the method that makes the striker move left
+	 */
 	@Override
 	public void moveLeft() {
 		if (getPlayerPosition().x - 5 - movementStep > 0) {
 			setPlayerPosition(new Point(getPlayerPosition().x - movementStep, getPlayerPosition().y));
 		}
 	}
-
+	/**
+	 * the method that makes the striker move right
+	 */
 	@Override
 	public void moveRight() {
 		if (getPlayerPosition().x + 50 + movementStep < 600) {
 			setPlayerPosition(new Point(getPlayerPosition().x + movementStep, getPlayerPosition().y));
 		}
 	}
-
+	
+	/**
+	 * the method that makes the striker move up
+	 */
 	@Override
 	public void moveUp() {
 		if (getPlayerPosition().y - 5 > 0) {
 			setPlayerPosition(new Point(getPlayerPosition().x, getPlayerPosition().y - 5));
 		}
 	}
-
+	/**
+	 * the method that makes the striker move down
+	 */
 	@Override
 	public void moveDown() {
 		if (getPlayerPosition().y + 50 < 300) {
 			setPlayerPosition(new Point(getPlayerPosition().x, getPlayerPosition().y + 5));
 		}
 	}
-
+	
+	/**
+	 * the method that makes the ball move when the the striker hits the ball
+	 */
 	@Override
 	public void shootBall() {
 		SoccerBall.getSoccerBall().moveBall(-20, -5.0, -0.05);
 	}
-
+	
+	/**
+	 * the method that makes the goalkeeper move randomly in a gaussian distribution fashion
+	 */
 	public void moveRandomly() {
 		Random random = new Random();
 		double playerCurrentXPosition = (double) getPlayerPosition().x + 15;
@@ -58,12 +78,17 @@ public class Goalkeeper extends GamePlayer {
 			moveRight();
 		}
 	}
-
+	
+	/**
+	 * sets the goalkeeper's position the default position of the goalkeeper
+	 */
 	@Override
 	public void setInitialPosition() {
 		setPlayerPosition(new Point(280, 70));
 	}
-
+	/**
+	 * the method that displays the statistics of the goalkeeper when the game is over
+	 */
 	@Override
 	public String toString() {
 		return playerName + " caught " + playerStatistics.toString() + " balls";
